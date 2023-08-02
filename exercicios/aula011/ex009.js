7ccffa73b13cad2ea33e8f8f8687119ea7cc3f0c
Felipe Elias de Morais <120696999+FelipeMorais19@users.noreply.github.com>
2023-08-02 00:22:07 -0300

    Até a aula 011

---
+++ exercicios/aula011/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'BR'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil' || pais == 'BR'){
+    console.log('Você é brasileiro!')
+}
+else{
+    console.log('Você é estrangeiro!')
+}
