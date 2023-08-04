7f00ffea5331c739a41a07ab3bf1dcaa5956a59d
Felipe Elias de Morais <120696999+FelipeMorais19@users.noreply.github.com>
2023-08-04 00:04:36 -0300

    .

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -1,3 +1,12 @@
 function verificar() {
-    window.alert('FUNCIONOU! ')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fAno.value.length == 0 || fAno.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    }
+    else {
+        window.alert('Tudo Ok')
+    }
 }   
