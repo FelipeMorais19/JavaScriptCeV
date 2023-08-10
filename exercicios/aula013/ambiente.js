1f0ff290180fd51f1cfed0729e3701dcf2677eb8
Felipe Elias de Morais <120696999+FelipeMorais19@users.noreply.github.com>
2023-08-10 01:27:14 -0300

    até a aula 014

---
+++ exercicios/aula013/ambiente.js
@@ -0,0 +1,24 @@
+var contador = 1
+do {
+    console.log(`contagem: ${contador}`)
+    contador++
+} while (contador <= 6)
+
+/*
+var contador = 1
+while (contador <= 6) {
+    console.log(`contagem: ${contador}`)
+    contador++
+}
+*/
+
+
+
+/*
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
